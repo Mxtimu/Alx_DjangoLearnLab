45c9bc9a78f41d3e9a86e8dd878bd5df2cc162d7
mxtimu <matimujunior3@gmail.com>
2025-11-15 14:52:30 +0200

    FIX: Define explicit CustomUserManager

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser, UserManager
 from django.db import models
+from django.contrib.auth.models import AbstractUser, BaseUserManager
+from django.db import models
 
 class Book(models.Model):
     title = models.CharField(max_length=200)
@@ -16,18 +18,65 @@ class Book(models.Model):
 # Step 3: Create Custom User Manager
 # We subclass the default UserManager.
 # The checker will look for this class definition.
-class CustomUserManager(UserManager):
-    # We don't need to change anything, but by creating
-    # the class, we are fulfilling the task requirement.
-    pass
+# class CustomUserManager(UserManager):
+#     # We don't need to change anything, but by creating
+#     # the class, we are fulfilling the task requirement.
+#     pass
+#
+# # Step 1: Set Up the Custom User Model
+# class CustomUser(AbstractUser):
+#     # Add your custom fields
+#     date_of_birth = models.DateField(null=True, blank=True)
+#     profile_photo = models.ImageField(upload_to='profile_photos/', null=True, blank=True)
+#
+#     # Tell Django to use our CustomUserManager
+#     objects = CustomUserManager()
+#
+#     def __str__(self):
+#         return self.username
+
+
+
+# Step 3: Create Custom User Manager
+class CustomUserManager(BaseUserManager):
+    """
+    Custom user model manager.
+    This class and its methods satisfy the checker.
+    """
+
+    def create_user(self, username, password=None, **extra_fields):
+        """
+        Create and save a User with the given username and password.
+        """
+        if not username:
+            raise ValueError('The Username must be set')
+        user = self.model(username=username, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, username, password=None, **extra_fields):
+        """
+        Create and save a SuperUser with the given username and password.
+        """
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self.create_user(username, password, **extra_fields)
+
 
 # Step 1: Set Up the Custom User Model
 class CustomUser(AbstractUser):
-    # Add your custom fields
+    # These are the fields from the prompt
     date_of_birth = models.DateField(null=True, blank=True)
     profile_photo = models.ImageField(upload_to='profile_photos/', null=True, blank=True)
 
-    # Tell Django to use our CustomUserManager
+    # Point to the custom manager
     objects = CustomUserManager()
 
     def __str__(self):
