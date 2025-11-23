017d8377e6966b060e28dece2873dca480acda6f
mxtimu <matimujunior3@gmail.com>
2025-11-23 19:33:13 +0200

    Implement Token Authentication and Permissions (Task 3)

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include # <--- Import include
 from rest_framework.routers import DefaultRouter # <--- Import DefaultRouter
 from .views import BookList, BookViewSet
+from rest_framework.authtoken.views import obtain_auth_token
 
 # this is done to Create a router instance
 router = DefaultRouter()
@@ -12,7 +13,6 @@ router.register(r'books_all', BookViewSet, basename='book_all')
 urlpatterns = [
 
     path('books/', BookList.as_view(), name='book-list'),
-
-    # Include the router URLs for BookViewSet
     path('', include(router.urls)),
+    path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
 ]
