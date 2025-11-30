7965d79c5d189200070a7ab7bae7e360bbad6236
mxtimu <matimujunior3@gmail.com>
2025-11-30 18:05:04 +0200

    i detted up a new django project, task 0

---
+++ advanced-api-project/api/serializers.py
@@ -0,0 +1,24 @@
+from rest_framework import serializers
+from .models import Book, Author
+from datetime import datetime
+
+class BookSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Book
+        fields = '__all__'
+
+    def validate_publication_year(self, value):
+        current_year = datetime.now().year
+        if value > current_year:
+            raise serializers.ValidationError("Publication year cannot be in the future.")
+        return value
+
+class AuthorSerializer(serializers.ModelSerializer):
+    books = BookSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Author
+        fields = ['name', 'books']
+
+# The AuthorSerializer includes a nested BookSerializer to show all books related to an author.
+# This is a read-only field, meaning you cannot create books directly through the author endpoint.
